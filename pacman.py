44721dc3d88caf3207ed2ccf7aa1b50ca2023593
Prakhar-FF13 <prakhar.meerut@gmail.com>
2018-06-20 23:16:45 +0530

    Movement for Pacman

--- pacman.py
+++ pacman.py
@@ -1,6 +1,6 @@
 from random import choice
 from turtle import *
-from base import vector,floor
+from base import vector,floor,square
 
 state = { 'score':0 }
 path = Tortle(visible = False)
@@ -40,14 +40,15 @@ tiles = [
 ]
 
 def square(x, y):
-    # Drawing a square (tile)
     path.up()
     path.goto(x,y)
     path.down()
     path.begin_fill()
+
     for count in range(4):
         path.forward(20)
-        path.lef(90)
+        path.left(90)
+
     path.end_fill()
 
 def offset(point):
@@ -65,3 +66,37 @@ def valid(point):
         return False
     return ((point.x % 20 == 0) or (point.y % 20 == 0))
 
+def world():
+    bg.color('black')
+    path.color('blue')
+
+    for index in range(len(tiles)):
+        tile = tiles[index]
+        if tile > 0:
+            x = (index % 20) * 20 - 200
+            y = 180 - (index // 20) * 20
+            square(x,y)
+            if tile == 1:
+                path.up()
+                path.goto(x + 10, y + 10)
+                path.dot(2, ' white')
+
+
+def move():
+    writer.undo()
+    writer.write(state['score'])
+    clear()
+    if valid(pacman + aim):
+        pacman.move(aim)
+
+    index = offset(pacman)
+    if tiles[index] == 1:
+        tiles[index] = 2
+        state['score'] += 1
+        x = (index % 20) * 20 - 200
+        y = 180 - (index // 20) * 20
+        square(x, y)
+    up()
+    goto(pacman.x + 10, pacman.y + 10)
+    dot(20, 'yellow')
+            
